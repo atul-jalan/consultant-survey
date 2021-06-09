04b33dce628d7bb7e2a02c7df198240350693dbd
Atul Jalan <atuljalan17@gmail.com>
2021-06-08 20:25:05 -0500

    finished constructing data correctly

--- pages/results/[id].js
+++ pages/results/[id].js
@@ -4,9 +4,12 @@ import fire from '../../config/fire-config';
 const useResults = ({ id }) => {
     const [data, setData] = useState({})
     const [loading, setLoading] = useState(true)
+    const [percentDist, setPercentDist] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    const [careerDist, setCareerDist] = useState([0, 0, 0, 0, 0, 0, 0])
 
     useEffect(() => {
         const getData = async () => {
+            // retrieve data from firestore
             const snapshot = await fire.firestore().collection('responses').get()
 
             const documents = {}
@@ -15,17 +18,38 @@ const useResults = ({ id }) => {
             });
 
             setData(documents)
+
+            // construct percentage distributions array
+            const values = Object.values(documents)
+            const pDist = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+            for (let i = 0; i < values.length; i += 1) {
+                const idx = Math.floor(values[i].RESULT / 10)
+                pDist[idx] += 1
+            }
+
+            setPercentDist(pDist)
+
+            // construct career plans distributions array
+            const cDist = [0, 0, 0, 0, 0, 0, 0]
+            for (let i = 0; i < values.length; i += 1) {
+                cDist[values[i].ANSWER] += 1
+            }
+
+            setCareerDist(cDist)
+
+            // set loading to finished
             setLoading(false)
         }
         getData();
     }, [])
 
-    return [data, loading];
+    return [data, percentDist, careerDist, loading];
 }
 
 const Results = ({ id }) => {
 
-    const [data, isLoading] = useResults({id});
+    const [data, percentageDistribution, careerDistribution, isLoading] = useResults({id});
 
     if (isLoading) return <p>Loading...</p>
     
